94878949fdf9229485d67fd64f05ff2a3cc17b0c
danishq <dqureshi08@gmail.com>
2025-02-02 15:47:52 +0530

    empty

--- Core Java/lec64.java
+++ Core Java/lec64.java
@@ -7,14 +7,7 @@ public class lec64 {
         // Integer num1 = new Integer(num); This is boxing
         System.out.println(num1);
 
-        // make update new : 3 : 15 pm
-
-        // another update 3:27 pm
         
-        // this is another update to ensure everything is working fine
-
-
-        // i should start udemy now 3 46 pm
     }
 }
 /* 
