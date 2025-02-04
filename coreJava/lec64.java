57c38b37af56b8e4ce9d0a6e30ddffaeffe22605
danishq <dqureshi08@gmail.com>
2025-02-05 00:09:04 +0530

    delete line lec64 windows

--- coreJava/lec64.java
+++ coreJava/lec64.java
@@ -11,12 +11,8 @@ public class lec64 {
 
         System.out.println(num);
 
-        System.out.println("hello");
-
-        System.out.println("second hello");
-
-        System.out.println("third hello");
         
+
     }
 }
 /* 
