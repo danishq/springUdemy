51ee452a79fa1cf577375db53b7c8fd1b70c04bf
danishq <dqureshi08@gmail.com>
2025-02-06 12:35:35 +0530

    Play Quiz Project lec 65-69

---
+++ advanceJava/p1_QuestionService/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] var0) {
+        QuestionService play = new QuestionService();
+        play.playQuiz();
+        play.printScore();
+    }
+        
+}
