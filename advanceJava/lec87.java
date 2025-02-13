b47823b05f4aa1a22fbca9c076d5ee3d8da4f036
danishq <dqureshi08@gmail.com>
2025-02-13 11:05:02 +0530

    remove class

--- advanceJava/lec87.java
+++ advanceJava/lec87.java
@@ -1,13 +1,6 @@
 // Try with multiple catch
 class lec87{
     public static void main(String[] args) {
-        int i=0;
-        int j=0;
-        try {
-           j=8/i; 
-        } catch (Exception e) {
-        System.out.println("something happened"+e);}    
-        
         
     }
 }
