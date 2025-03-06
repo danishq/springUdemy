f7a722c55feb1585bbe72cb7f28e129dfc4c88b2
danishq <dqureshi08@gmail.com>
2025-03-07 02:13:50 +0530

    java changes

--- advanceJava/lec88.java
+++ advanceJava/lec88.java
@@ -3,21 +3,4 @@ class lec88{
     public static void main(String[] args) {
         
     }
-
 }
-/*
-    Parent class -> object class
-    |
-    v
-    Throwable
-        1. Error
-            a. Thread Death b. IO Error c. Out of Memory
-        2. Exception
-            a. Runtime exception b. SQL execption c. IO execption
-            a. Runtime
-                i. Arithmetic exp
-                ii. Arrya index out of bound
-                iii. Null POINT exp
-            -- These are Unchecked Exception
-    
-*/
